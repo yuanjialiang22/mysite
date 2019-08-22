518b8ef341ad2982f959fb044b32fb4811a85c28
abc <abc@qbc.com>
2019-08-22 15:26:52 +0800

    Django V2.2

--- lib/urls.py
+++ lib/urls.py
@@ -2,7 +2,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'lib'	#添加这行
 urlpatterns = [
     path('', views.index, name='index'),
 	path('detail/', views.detail, name='detail'),
+	path('addBook/', views.addBook, name='addBook'),
+	path('delBook/<int:book_id>', views.deleteBook, name='delBook'),
 ]
